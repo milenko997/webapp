9cf8b0f3adf366a1acfbd6fc5a5df4e4243ba5c2
milenko997 <48036062+milenko997@users.noreply.github.com>
2019-10-08 10:57:54 +0200

    adding new faq page and error 404 page

--- main.go
+++ main.go
@@ -17,9 +17,22 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\"> support@lenslocked.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Welcome to FAQ page!")
+}
+
+func err(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "ERROR 404, PAGE NOT FOUND YOU FOOL")
+}
+
 func main() {
+	var h http.Handler = http.HandlerFunc(err)
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = h
 	http.ListenAndServe(":3000", r)
 }
