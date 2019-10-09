fa292ea410cf27c7340fbeb9bc1b64276c8126cd
milenko997 <48036062+milenko997@users.noreply.github.com>
2019-10-09 10:08:43 +0200

    changed router

--- main.go
+++ main.go
@@ -4,35 +4,34 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/julienschmidt/httprouter"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
-func contact(w http.ResponseWriter, r *http.Request) {
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\"> support@lenslocked.com</a>.")
 }
-
-func faq(w http.ResponseWriter, r *http.Request) {
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "Welcome to FAQ page!")
+	fmt.Fprint(w, "<h1>Welcome to FAQ page</h1>")
 }
 
-func err(w http.ResponseWriter, r *http.Request) {
+func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "ERROR 404, PAGE NOT FOUND YOU FOOL")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>ERROR 404, PAGE NOT FOUND</h1>")
 }
 
 func main() {
-	var h http.Handler = http.HandlerFunc(err)
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	r.NotFoundHandler = h
+	r := httprouter.New()
+	r.GET("/", home)
+	r.GET("/contact", contact)
+	r.GET("/faq", faq)
+	r.NotFound = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
